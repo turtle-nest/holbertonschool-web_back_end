e8cfd5c17a256f86320c74a6cf1d8fd19f98d7b6
turtle-nest <n78693107@gmail.com>
2025-03-24 10:55:44 +0100

    Add babel.config.js and .eslintrc.js

---
+++ ES6_basic/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    env: {
+      browser: false,
+      es2021: true,
+      node: true,
+      jest: true,
+    },
+    extends: ['eslint:recommended'],
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    rules: {
+      // Add specific rules here if requested
+    },
+  };
+  
--- ES6_basic/README.md
+++ ES6_basic/README.md
@@ -1,1 +1,3 @@
 # ES6 Basics
+
+Project covering ES6 features like `const`, `let`, arrow functions, etc.
