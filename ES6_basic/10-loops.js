4c315cba1c6155ec8120e8d86cea032b02066f81
turtle-nest <n78693107@gmail.com>
2025-03-26 00:14:00 +0100

    Add task 10

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,6 @@
+export default function appendToEachArrayValue(array, appendString) {
+  for (const [index, value] of array.entries()) {
+    array[index] = appendString + value;
+  }
+  return array;
+}
