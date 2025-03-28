da6e77ea1d89b1c295b5b8de55d1c64673907f7a
turtle-nest <n78693107@gmail.com>
2025-03-28 19:36:16 +0100

    Fix tabs

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,18 +1,18 @@
 class Building {
-    constructor(sqft) {
-      if (typeof sqft !== 'number') {
-        throw new TypeError('Square footage must be a number');
-      }
-      this._sqft = sqft;
-  
-      if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-      }
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Square footage must be a number');
     }
-  
-    get sqft() {
-      return this._sqft;
+    this._sqft = sqft;
+
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
-  
-  export default Building;
+
+  get sqft() {
+    return this._sqft;
+  }
+}
+
+export default Building;
