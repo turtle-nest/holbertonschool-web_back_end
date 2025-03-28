2ffc93a8d02e623dc57600227afc252cebb48510
turtle-nest <n78693107@gmail.com>
2025-03-28 19:38:13 +0100

    Fix tabs

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,11 +1,6 @@
 import Building from './5-building.js';
 
 export default class SkyHighBuilding extends Building {
-  /**
-   * Constructor for SkyHighBuilding
-   * @param {number} sqft - The total square footage (passed to parent)
-   * @param {number} floors - Number of floors
-   */
   constructor(sqft, floors) {
     super(sqft);
 
@@ -13,18 +8,13 @@ export default class SkyHighBuilding extends Building {
       throw new TypeError('floors must be a number');
     }
 
-    this._floors = floors;
-  }
+  this._floors = floors;
+}
 
-  // Getter for floors
   get floors() {
     return this._floors;
   }
 
-  /**
-   * Overridden method to provide evacuation instructions
-   * @returns {string}
-   */
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors`;
   }
