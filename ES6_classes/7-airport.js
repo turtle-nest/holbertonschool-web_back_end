3a15b81617afc7e88d6da8bcfbf1b44349424ba9
turtle-nest <n78693107@gmail.com>
2025-03-28 19:44:08 +0100

    Fix tabs

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,37 +1,24 @@
 export default class Airport {
-    /**
-     * Constructor for Airport
-     * @param {string} name - Full name of the airport
-     * @param {string} code - IATA airport code
-     */
-    constructor(name, code) {
-      if (typeof name !== 'string') {
-        throw new TypeError('Name must be a string');
-      }
-  
-      if (typeof code !== 'string') {
-        throw new TypeError('Code must be a string');
-      }
-  
-      this._name = name;
-      this._code = code;
+  constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
     }
-  
-    // Getter for name
-    get name() {
-      return this._name;
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
     }
-  
-    // Getter for code
-    get code() {
-      return this._code;
-    }
-  
-    /**
-     * Override the default toString method
-     * @returns {string}
-     */
-    toString() {
-      return `[object ${this._code}]`;
-    }
+    this._name = name;
+    this._code = code;
   }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
