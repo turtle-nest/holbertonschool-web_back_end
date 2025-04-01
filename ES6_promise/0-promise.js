4e60cb288faf8b5e03538f895f48da61ca679735
turtle-nest <n78693107@gmail.com>
2025-04-01 11:48:13 +0200

    Fix task 0

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,7 +1,5 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    // Here we could perform asynchronous processing
-    // But for now, we just return an "empty" promise
     resolve();
   });
 }
