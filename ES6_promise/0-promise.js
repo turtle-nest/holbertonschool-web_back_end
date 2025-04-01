2b682a318a4828c80f6ca212e92cf69d12b51d4c
turtle-nest <n78693107@gmail.com>
2025-04-01 11:47:00 +0200

    Add task 0

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Here we could perform asynchronous processing
+    // But for now, we just return an "empty" promise
+    resolve();
+  });
+}
