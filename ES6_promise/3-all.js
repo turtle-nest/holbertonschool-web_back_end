9d8c5bad507b90d4260c5d841e8e695df96bd2e8
turtle-nest <n78693107@gmail.com>
2025-04-01 15:01:05 +0200

    Correction of task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,9 +3,7 @@ import { createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const photo = results[0];
-      const user = results[1];
+    .then(([photo, user]) => {
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
