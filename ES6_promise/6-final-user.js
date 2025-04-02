ac7a001ba1aa2e67a1503f9b08f23572083def0a
turtle-nest <n78693107@gmail.com>
2025-04-02 09:13:01 +0200

    Change the implementation of task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,20 +2,13 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUp = signUpUser(firstName, lastName);
-  const upload = uploadPhoto(fileName);
-
-  return Promise.allSettled([signUp, upload])
-    .then((results) => results.map((res) => {
-      if (res.status === 'fulfilled') {
-        return {
-          status: res.status,
-          value: res.value,
-        };
-      }
-      return {
-        status: res.status,
-        value: res.value,
-      };
-    }));
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) =>
+    results.map((res) => ({
+      status: res.status,
+      value: res.status === 'fulfilled' ? res.value : res.reason,
+    }))
+  );
 }
