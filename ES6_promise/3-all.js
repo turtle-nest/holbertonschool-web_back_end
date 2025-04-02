3ed68ffd6a1772e3bc23a5c2fb0d91424cc57ef0
turtle-nest <n78693107@gmail.com>
2025-04-02 08:43:32 +0200

    Another try to pass task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,10 +3,8 @@ import { createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((res) => {
-      const { body } = res[0];
-      const { firstName, lastName } = res[1];
-      console.log(`${body} ${firstName} ${lastName}`);
+    .then((responses) => {
+      console.log(`${responses[0].body} ${responses[1].firstName} ${responses[1].lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
