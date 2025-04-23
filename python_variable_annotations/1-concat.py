f11f9375a712ef9cd3c705399cb36070588fb06f
turtle-nest <n78693107@gmail.com>
2025-04-23 15:56:33 +0200

    1. Complete the task

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" A module that contains concat function """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ A function that concatenates two strings """
+    return str1 + str2
