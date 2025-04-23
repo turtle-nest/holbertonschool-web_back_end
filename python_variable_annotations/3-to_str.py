f23da381095db080a1fc83338efe0c08f045e46b
turtle-nest <n78693107@gmail.com>
2025-04-23 16:10:05 +0200

    3. Complete task

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" A module that contains to_str function """
+
+
+def to_str(n: float) -> str:
+    """ A function that return a str represenation of a float """
+    return str(n)
