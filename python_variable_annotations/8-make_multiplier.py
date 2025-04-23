0f91f2614104c00afc68408a5b7b8025794e12ad
turtle-nest <n78693107@gmail.com>
2025-04-23 18:57:07 +0200

    8. Fixed file

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -6,7 +6,7 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """ A type-annotated function make_multiplier that takes a float
-        multiplier as argument and returns a function 
+        multiplier as argument and returns a function
         that multiplies a float by multiplier
     """
     def multiplier_function(x: float) -> float:
