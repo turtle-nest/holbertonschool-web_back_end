87a43c4cebec0c7a78b7a1415f71a12d3e4fd165
turtle-nest <n78693107@gmail.com>
2025-04-23 15:49:46 +0200

    0. Complete task

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,4 +1,5 @@
-import { uploadPhoto, createUser } from './utils.js';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+def add(a: float, b: float) -> float:
+    """ A function that adds two numbers """
+    return a + b
