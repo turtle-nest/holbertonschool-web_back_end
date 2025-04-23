4de2cf21887c6443daa1d8ca09a0c16e6d748240
turtle-nest <n78693107@gmail.com>
2025-04-23 16:22:32 +0200

    5. Complete task

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" A module that contains sum_list function """
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ A type-annotated function sum_list which takes a list input_list
+        of floats as argument and returns their sum as a float
+    """
+    return sum(input_list)
