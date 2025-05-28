cee4214c593b54f6b1d04dc0e31ca1d0322a82f4
turtle-nest <n78693107@gmail.com>
2025-05-28 11:29:28 +0200

    4. Add task

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.statusCode = 200;
+  res.end('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+module.exports = app;
